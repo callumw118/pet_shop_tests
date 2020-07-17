f99b11df85eaeaae9f356f678f0a59a6c21c3484
Callum Wolfe <wolfe.callum@gmail.com>
2020-07-17 14:32:50 +0100

    Create add_pet_to_stock function

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -33,3 +33,15 @@ def find_pet_by_name(pet_shop, name):
         if name == pet["name"]:
             return pet
     return None
+
+# def remove_pet_by_name(pet_shop, name):
+#     pet_to_remove = find_pet_by_name(pet_shop, name)
+#     for pet in pet_shop["pets"]:
+#         if name == pet_to_remove["name"]:
+#             pet_shop["pets"].remove(pet)
+#     print(pet_shop["pets"])
+
+def add_pet_to_stock(pet_shop, new_pet):
+    pet_shop["pets"].append(new_pet)
+    
+
